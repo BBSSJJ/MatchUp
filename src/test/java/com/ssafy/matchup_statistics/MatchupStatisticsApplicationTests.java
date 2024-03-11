972c6774e7f82471e72825022527d745bbf92394
남상엽 <spearoad15@gmail.com>
2024-03-11 10:41:21 +0900

    S10P22A405-107-남상엽-statistics-server-구축하기 - statistics server springboot로 구축 - 해당 서버에 Riot에서 제공하는 API Data 저장 및 빅데이터 서버에게 제공

---
+++ src/test/java/com/ssafy/matchup_statistics/MatchupStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.matchup_statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchupStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
