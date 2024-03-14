262a85f907b6d931177feff643a1d51e08374e40
이연우 <ckadltmf0224@naver.com>
2024-03-14 11:09:40 +0900

    fix: 에러 수정 및 안 쓰는 파일 삭제

--- src/app/ui/article/create/create.tsx
+++ src/app/ui/article/create/create.tsx
@@ -1,27 +1,27 @@
-'use client'
+// 'use client'
 
-import { useEditor, EditorContent } from "@tiptap/react"
-import StarterKit from "@tiptap/starter-kit"
+// import { useEditor, EditorContent } from "@tiptap/react"
+// import StarterKit from "@tiptap/starter-kit"
 // import { Editor } from '@tiptap/core'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
+// import Document from '@tiptap/extension-document'
+// import Paragraph from '@tiptap/extension-paragraph'
+// import Text from '@tiptap/extension-text'
 
-export default function Create() {
-  const editor = useEditor({
-    extensions: [
-      // Document,
-      // Paragraph,
-      // Text,
-      StarterKit,
-    ],
-    content: '<h1>글 작성을 위한 페이지입니다.</h1>',
-    // autofocus: true,
-    // editable: true,
-    // injectCSS: false,
-  })
+// export default function Create() {
+//   const editor = useEditor({
+//     extensions: [
+//       // Document,
+//       // Paragraph,
+//       // Text,
+//       StarterKit,
+//     ],
+//     content: '<h1>글 작성을 위한 페이지입니다.</h1>',
+//     // autofocus: true,
+//     // editable: true,
+//     // injectCSS: false,
+//   })
 
-  return (
-    <EditorContent editor={editor} />
-  )
-}
+//   return (
+//     <EditorContent editor={editor} />
+//   )
+// }
