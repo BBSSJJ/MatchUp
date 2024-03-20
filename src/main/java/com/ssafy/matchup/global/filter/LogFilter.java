75275440c875a79a7364e242c580959e35d11e6c
배성준 <bdhbsj@gmail.com>
2024-03-20 11:24:25 +0900

    feat : API Gateway 테스트용 LogFilter 적용

---
+++ src/main/java/com/ssafy/matchup/global/filter/LogFilter.java
@@ -0,0 +1,33 @@
+package com.ssafy.matchup.global.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LogFilter extends AbstractGatewayFilterFactory<LogFilter.Config> {
+    public LogFilter(){
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            log.info("PRE filter / request uri : {}", request.getURI());
+
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+
+    }
+
+}
