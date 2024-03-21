f30ba1f6002924e834e1653636fb7361c555bca1
남상엽 <spearoad15@gmail.com>
2024-03-21 17:55:46 +0900

    S10P22A405-168-남상엽-사용자-통계-db에-저장 - 소환사 지표 저장기능 구현 완료 - 이제 사용자는 본인의 아이디로 소환사 지표를 획득할 수 있다.

--- src/main/java/com/ssafy/matchup_statistics/indicator/service/SummonerIndicatorService.java
+++ src/main/java/com/ssafy/matchup_statistics/indicator/service/SummonerIndicatorService.java
@@ -1,4 +1,33 @@
 package com.ssafy.matchup_statistics.indicator.service;
 
+import com.ssafy.matchup_statistics.indicator.entity.SummonerIndicator;
+import com.ssafy.matchup_statistics.indicator.entity.riot.league.LeagueIndicator;
+import com.ssafy.matchup_statistics.indicator.entity.riot.match.MatchIndicator;
+import com.ssafy.matchup_statistics.indicator.service.builder.LeagueIndicatorBuilder;
+import com.ssafy.matchup_statistics.indicator.service.builder.MatchIndicatorBuilder;
+import com.ssafy.matchup_statistics.summoner.api.SummonerRestApi;
+import com.ssafy.matchup_statistics.summoner.api.dto.response.SummonerInfoResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class SummonerIndicatorService {
+
+    private final SummonerRestApi summonerRestApi;
+    private final LeagueIndicatorBuilder leagueIndicatorBuilder;
+    private final MatchIndicatorBuilder matchIndicatorBuilder;
+
+    public SummonerIndicator createSummonerIndicatorBySummonerName(String summonerName) {
+        SummonerInfoResponseDto summonerInfo = summonerRestApi.getSummonerInfoResponseDtoByName(summonerName);
+        LeagueIndicator leagueIndicator = leagueIndicatorBuilder.build(summonerInfo.getId());
+        List<MatchIndicator> matchIndicators = matchIndicatorBuilder.buildMatches(summonerInfo.getPuuid());
+
+        SummonerIndicator summonerIndicator = new SummonerIndicator(summonerInfo.getId(), matchIndicators, leagueIndicator);
+        summonerIndicator.eraseUnnecessarilyField();
+
+        return summonerIndicator;
+    }
 }
--- src/main/java/com/ssafy/matchup_statistics/summoner/service/SummonerService.java
+++ src/main/java/com/ssafy/matchup_statistics/summoner/service/SummonerService.java
@@ -1,22 +1,24 @@
 package com.ssafy.matchup_statistics.summoner.service;
 
+import com.ssafy.matchup_statistics.indicator.entity.SummonerIndicator;
+import com.ssafy.matchup_statistics.indicator.service.SummonerIndicatorService;
 import com.ssafy.matchup_statistics.summoner.entity.Summoner;
 import com.ssafy.matchup_statistics.summoner.service.sub.SummonerRegistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SummonerService {
-    private final SummonerRegistService summonerRegistService;
 
-    public Summoner getSummonerDto(String name) {
-        summonerRegistService.registSummonerInfo(name);
-        return summonerRegistService.getSummonerByName(name);
+    private final SummonerIndicatorService summonerIndicatorService;
+    private final MongoTemplate mongoTemplate;
+
+    public SummonerIndicator getNewSummonerIndicator(String name) {
+        SummonerIndicator summonerIndicator = summonerIndicatorService.createSummonerIndicatorBySummonerName(name);
+        mongoTemplate.save(summonerIndicator);
+        return summonerIndicator;
     }
-
-//    public SummonerIndicator getSummonerIndicatorBySummonerName(String summonerName) {
-//        summonerCreateIndicatorService.makeNewSummonerIndicatorBySummonerName(summonerName);
-//    }
 
 }
