0c5bab3c13a15276fc4968794fed906bd6f983b9
sumin <sumin510@naver.com>
2024-03-22 02:03:16 +0900

    feat: 게시글 작성 기능
    에디터 customize
    게시글 fetch함수 작성
    컴포넌트명 수정

--- src/app/ui/login-button.tsx
+++ src/app/ui/login-button.tsx
@@ -2,22 +2,32 @@
 
 import React,  { useState, MouseEvent } from 'react';
 import styles from './login-button.module.css';
-export const SERVER_URL = process.env.SERVER_URL;
+export const SERVER_URL = process.env.SERVER_URL || "http://70.12.247.47:9000";
 
 const LoginButton = ({ snsType } : {snsType :string} ) => {
-    const REDIRECT_URI = `http://70.12.247.47:9000/api/oauth2/${snsType}`
-
+    const REDIRECT_URI = `${SERVER_URL}/api/oauth2/${snsType}`
+    
     const onClick = async () => {
         try {
             window.location.href = REDIRECT_URI;
          
         } catch(error) {
-            console.log(error);
+            console.error(error);
         }
     }
+
+    const snsTypeClassMap: { [key: string]: string } = {
+        kakao: styles.kakaoButton,
+        naver: styles.naverButton,
+        google: styles.googleButton,
+    };
+
+    const buttonClassName = snsTypeClassMap[snsType]
+
+
     return (
-        <div className={styles.button}>
-            <a href="#" onClick={onClick}>{snsType} login</a>
+        <div className={buttonClassName} onClick={onClick}>
+            <a href="#" ></a>
         </div>
     )
 }
