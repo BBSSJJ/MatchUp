7f163d80d201d65a257b80ce66cd1d6b5b2eeb62
남상엽 <spearoad15@gmail.com>
2024-03-25 17:08:08 +0900

    fix : BASE_PORT 삭제

--- view/src/views/UploadView.vue
+++ view/src/views/UploadView.vue
@@ -2,7 +2,6 @@
 import axios from 'axios'
 import { ref, onBeforeMount } from 'vue'
 const FILESERVER_BASE_URL = 'https://matchup.site/file'
-const FILESERVER_PORT = 8001
 const upload = async () => {
   const files = document.getElementById('uploadFile').files
 
