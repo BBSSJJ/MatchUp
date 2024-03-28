b2b93ada46a06dd5cbf0388057e70c8fca6b93a7
배성준 <bdhbsj@gmail.com>
2024-03-28 19:12:14 +0900

    feat : 댓글 log 임시 추가

--- src/main/java/com/ssafy/matchup/mz/comment/dto/request/RegistCommentRequestDto.java
+++ src/main/java/com/ssafy/matchup/mz/comment/dto/request/RegistCommentRequestDto.java
@@ -1,8 +1,10 @@
 package com.ssafy.matchup.mz.comment.dto.request;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class RegistCommentRequestDto {
     private String content;
     private Long parentCommentId;
