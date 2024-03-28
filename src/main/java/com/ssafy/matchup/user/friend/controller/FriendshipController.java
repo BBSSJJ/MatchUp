714b839f311435a5e00f29f35cf2dedab1e833f2
배성준 <bdhbsj@gmail.com>
2024-03-28 17:21:53 +0900

    feat : 친구 관리 API 구현

--- src/main/java/com/ssafy/matchup/user/friend/controller/FriendshipController.java
+++ src/main/java/com/ssafy/matchup/user/friend/controller/FriendshipController.java
@@ -3,8 +3,10 @@ package com.ssafy.matchup.user.friend.controller;
 import com.ssafy.matchup.global.dto.ListDto;
 import com.ssafy.matchup.global.dto.MessageDto;
 import com.ssafy.matchup.global.util.JwtTokenUtil;
-import com.ssafy.matchup.user.friend.dto.FriendRequestDto;
+import com.ssafy.matchup.user.friend.entity.FriendStatus;
 import com.ssafy.matchup.user.friend.service.FriendshipService;
+import com.ssafy.matchup.user.main.dto.UserDto;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,25 +23,31 @@ public class FriendshipController {
     private final JwtTokenUtil jwtTokenUtil;
     private final FriendshipService friendshipService;
 
+    @Operation(summary = "친구 목록 조회", description = "쿼리 파라미터로 FRIEND, SENT, RECEIVED 필수로 넣어서 친구 목록을 조회합니다.")
     @GetMapping
-    public ResponseEntity<?> showFriendList(HttpServletRequest request) {
-
-        return new ResponseEntity<>(new ListDto<>(friendshipService.showFriendsList(jwtTokenUtil.getUserId(request))), HttpStatus.OK);
+    public ResponseEntity<ListDto<UserDto>> showFriendList(HttpServletRequest request, @RequestParam(value = "friendStatus") FriendStatus friendStatus) {
+        return new ResponseEntity<>(new ListDto<>(friendshipService.showFriendList(jwtTokenUtil.getUserId(request), friendStatus)), HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<?> registFriendship(HttpServletRequest request, @RequestBody FriendRequestDto friendRequestDto) {
-
-        friendshipService.registFriendship(jwtTokenUtil.getUserId(request), friendRequestDto.getFriendId());
-
-        return new ResponseEntity<>(new MessageDto("regist"), HttpStatus.OK);
+    @Operation(summary = "친구 요청", description = "친구 요청하는 API 입니다.")
+    @PostMapping("/{friend-id}")
+    public ResponseEntity<?> requestFriendship(HttpServletRequest request, @PathVariable("friend-id") Long friendId) {
+        friendshipService.sendFriendRequest(jwtTokenUtil.getUserId(request), friendId);
+        return new ResponseEntity<>(new MessageDto("send friend request success"), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{friendId}")
-    public ResponseEntity<?> deleteFriendship(HttpServletRequest request, @PathVariable Long friendId) {
+    @Operation(summary = "친구 요청", description = "친구 수락하는 API 입니다.")
+    @PatchMapping("/{friend-id}")
+    public ResponseEntity<?> acceptFriendship(HttpServletRequest request, @PathVariable("friend-id") Long friendId) {
+        friendshipService.allowFriendRequest(jwtTokenUtil.getUserId(request), friendId);
+        return new ResponseEntity<>(new MessageDto("allow friend request"), HttpStatus.OK);
 
+    }
+
+    @Operation(summary = "친구 삭제, 거절", description = "친구를 삭제하거나 요청을 거절하는 API 입니다.")
+    @DeleteMapping("/{friend-id}")
+    public ResponseEntity<?> deleteFriendship(HttpServletRequest request, @PathVariable("friend-id") Long friendId) {
         friendshipService.deleteFriendship(jwtTokenUtil.getUserId(request), friendId);
-
         return new ResponseEntity<>(new MessageDto("deleted"), HttpStatus.OK);
     }
 }
