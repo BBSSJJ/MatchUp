03ae48476d509b6cf1e4d8a3423493889b9ed632
배성준 <bdhbsj@gmail.com>
2024-03-28 18:17:29 +0900

    init alarmserver

---
+++ src/test/java/com/ssafy/matchup_alarm/MatchupAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.matchup_alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchupAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
