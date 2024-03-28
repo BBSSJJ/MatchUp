39b0d4a8c3dfdcf2055d19e0f346e802f267d2d9
이원석 <epmdrpl213@gmail.com>
2024-03-28 12:25:38 +0900

    feat : api 경로 설정

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 matchup = FastAPI()
 
 
-@matchup.get("/recommendation_1/")
-async def recommendation_1():
-    return {"users": [1, 2, 3, 4, 5]}
+@matchup.get("/api/recommend/1/{user_pk}")
+async def recommendation_1(user_pk: int):
+    return {"users": [1, 2, 3, 4, 5, user_pk]}
 
 
-@matchup.get("/recommendation_2/")
-async def recommendation_2():
-    return {"users": [6, 7, 8, 9, 10]}
+@matchup.get("/api/recommend/2/{user_pk}")
+async def recommendation_2(user_pk: int):
+    return {"users": [6, 7, 8, 9, 10, user_pk]}
