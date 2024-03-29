4cba3449fdc5528a04bf665e1b5e2e63d7c0457f
이연우 <ckadltmf0224@naver.com>
2024-03-29 13:04:53 +0900

    fix: authAtom

--- src/store/authAtom.js
+++ src/store/authAtom.js
@@ -1,6 +1,7 @@
 import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils';
 
+
 // 로컬 스토리지에 초기값 저장
 export const isLoggedInAtom = atomWithStorage('isLoggedIn', false)
 export const userInfoAtom = atomWithStorage('user', null)
