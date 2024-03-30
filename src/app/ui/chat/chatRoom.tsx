28e7f625f038c16c8f593e3805c9eb001c14a654
sumin <sumin510@naver.com>
2024-03-31 03:26:29 +0900

    fix: 모달 오픈 함수호출

--- src/app/ui/chat/chatRoom.tsx
+++ src/app/ui/chat/chatRoom.tsx
@@ -7,7 +7,7 @@ import DirectMessage from "./chat";
 import ChatModal from "./chatModal";
 
 // 채팅목록에서 보이는 개별 채팅방 - 여기서 DM누르면 모달창이 뜸
-export default function ChatRoom({chatId, badge} :{ chatId :string; badge :number }) {
+export default function ChatRoom({chatId, badge, you} :{ chatId :string; badge :number; you :number[] }) {
     const [isRoomOpen, setIsRoomOpen] = useAtom(isRoomOpenAtom)
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
 	const [scrollBehavior, setScrollBehavior] = React.useState<ModalProps["scrollBehavior"]>("inside");
@@ -15,7 +15,7 @@ export default function ChatRoom({chatId, badge} :{ chatId :string; badge :numbe
 
     const handleChat = () => {
         setRoomId(chatId)
-        onOpen
+        onOpen()
     }
 
     return (
@@ -25,7 +25,8 @@ export default function ChatRoom({chatId, badge} :{ chatId :string; badge :numbe
                     <div className="flex gap-5">
                     <Avatar isBordered radius="full" size="md" src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/Leblanc.png" />
                     <div className="flex flex-col gap-1 items-start justify-center">
-                        <h4 className="text-small font-semibold leading-none text-default-600">소환사 이름</h4>
+                        <h4>chatId: {chatId}</h4>
+                        <h4 className="text-small font-semibold leading-none text-default-600">소환사명</h4>
                         <h5 className="text-tiny tracking-tight text-default-400">가장 최근 메시지</h5>
                     </div>
                     </div>
