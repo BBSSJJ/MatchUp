0089abecef2fc73bcb4e78aab84b6a0b0f380359
이준학 <0710june@naver.com>
2024-03-31 16:28:29 +0900

    fix: 채팅내역 조회 api 수정

--- src/main/java/com/ssafy/chat/ChatApplication.java
+++ src/main/java/com/ssafy/chat/ChatApplication.java
@@ -1,12 +1,15 @@
 package com.ssafy.chat;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@Slf4j
 public class ChatApplication {
 
     @PostConstruct
@@ -14,6 +17,15 @@ public class ChatApplication {
         // timezone 셋팅
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
+
+    @Value("${MATCHUP_CHAT_DB}")
+    private String matchupChatDb;
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info("Using MATCHUP_CHAT_DB: {}", matchupChatDb);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
