37a457279960a8d4d1b5369abfa8b2ef8d39dec2
sumin <sumin510@naver.com>
2024-03-31 14:02:33 +0900

    fix: 파트너 정보 받아서 소환사명 출력 오류

--- src/app/ui/chat/chat.tsx
+++ src/app/ui/chat/chat.tsx
@@ -66,7 +66,7 @@ export default function DirectMessage({roomId} : {roomId :string}) {
                     }));
                 });
             };
-
+			
             setStompClient(stomp);
 
         } catch (error) {
