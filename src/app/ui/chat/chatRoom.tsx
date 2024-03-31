a8a04a0de11c15d2d7a91552d5642ee19d9596cd
sumin <sumin510@naver.com>
2024-03-31 10:58:38 +0900

    fix: chat.tsx 수정

--- src/app/ui/chat/chatRoom.tsx
+++ src/app/ui/chat/chatRoom.tsx
@@ -1,10 +1,20 @@
 "use client"
 import React, { useState, useEffect } from "react";
+import useSWR, { mutate } from 'swr';
 import {Card, CardHeader, CardBody, CardFooter, Avatar, Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, ModalProps} from "@nextui-org/react";
 import { useAtom } from "jotai";
 import { isRoomOpenAtom, roomIdAtom } from '@/store/chatAtom'
 import DirectMessage from "./chat";
 import ChatModal from "./chatModal";
+import { SERVER_API_URL } from "@/utils/instance-axios";
+
+const youFetcher =async (url:string) => {
+    const response = await fetch(url); // 서버로부터 데이터 가져오기
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return response.json();
+}
 
 // 채팅목록에서 보이는 개별 채팅방 - 여기서 DM누르면 모달창이 뜸
 export default function ChatRoom({chatId, badge, you} :{ chatId :string; badge :number; you :number[] }) {
@@ -18,6 +28,22 @@ export default function ChatRoom({chatId, badge, you} :{ chatId :string; badge :
         onOpen()
     }
 
+    const {data: user , error: userError, isLoading: userLoading } = useSWR(
+        `${SERVER_API_URL}/api/users/${you}`,
+        youFetcher,
+        {
+          onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+            if (error.status === 401) return
+      
+          }, 
+          revalidateOnFocus: false,
+          revalidateOnMount: true,
+        }
+        // { refreshInterval: 1000 }
+      )
+    
+    
+
     return (
         <div className="z-20000">
             <Card className="max-w-[340px]">
@@ -25,7 +51,7 @@ export default function ChatRoom({chatId, badge, you} :{ chatId :string; badge :
                     <div className="flex gap-5">
                     <Avatar isBordered radius="full" size="md" src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/Leblanc.png" />
                     <div className="flex flex-col gap-1 items-start justify-center">
-                        <h4>{you}</h4>
+                        <h4>{user.riotAccount.summonerProfile.name}</h4>
                         <h4 className="text-[7px]">{chatId}</h4>
                         <h4 className="text-small font-semibold leading-none text-default-600">소환사명</h4>
                         <h5 className="text-tiny tracking-tight text-default-400">가장 최근 메시지</h5>
