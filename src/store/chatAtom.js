c7272a14b5b74dfb017b6c616dabbe7e4c1cca9f
sumin <sumin510@naver.com>
2024-03-31 14:32:55 +0900

    fix: RoomIdAtom local storage 저장

--- src/store/chatAtom.js
+++ src/store/chatAtom.js
@@ -2,4 +2,4 @@ import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils';
 
 export const isRoomOpenAtom = atom(false)
-export const roomIdAtom = atomWithStorage("")
+export const roomIdAtom = atomWithStorage("roomId","")
