9ee57f2fc1181cf239b82185c92e20f85b02c4df
sumin <sumin510@naver.com>
2024-03-31 14:32:06 +0900

    fix: RoomIdatom local storage 저장

--- src/app/ui/Sidebar.tsx
+++ src/app/ui/Sidebar.tsx
@@ -81,36 +81,38 @@ const SideBar: React.FC = () => {
 		}}>
 			{!isLoggedIn && <p>먼저 로그인하고 서비스를 이용하세요</p>}
 			{/* 로그인 하면 보이는 기능 */}
+			{isLoggedIn && 
 			<div>
-				{/* 토글버튼 */}
-				<Button className="w-[50px] h-[30px] min-w-0" color="warning" onPress={()=> handleToggle('f')}>친구</Button>
-				<Button className="w-[50px] h-[30px] min-w-0" color="warning" onPress={()=> handleToggle('c')}>채팅</Button>
-				{/* 친구목록 */}
-				<div className={chatOrFreiend ? "" : styles.hide}>
-					<span className={styles.title}>Friends</span>
-					<Button className="w-[10px] h-[15px] min-w-0" color="warning">friends</Button>
-					<Button className="w-[10px] h-[15px] min-w-0" color="warning">sent</Button>
-					<Button className="w-[10px] h-[15px] min-w-0" color="warning">requested</Button>
+			{/* 토글버튼 */}
+			<Button className="w-[50px] h-[30px] min-w-0" color="warning" onPress={()=> handleToggle('f')}>친구</Button>
+			<Button className="w-[50px] h-[30px] min-w-0" color="warning" onPress={()=> handleToggle('c')}>채팅</Button>
+			{/* 친구목록 */}
+			<div className={chatOrFreiend ? "" : styles.hide}>
+				<span className={styles.title}>Friends</span>
+				<Button className="w-[10px] h-[15px] min-w-0" color="warning">friends</Button>
+				<Button className="w-[10px] h-[15px] min-w-0" color="warning">sent</Button>
+				<Button className="w-[10px] h-[15px] min-w-0" color="warning">requested</Button>
 
-					<div className="flex flex-col gap-4 items-center z-20000">
-						<p>회원가입하고 나와 맞는 듀오를 찾아보세요</p>
-						<Friends />
-					</div>
-				</div>
-				{/* 채팅목록창 */}
-				<div className={chatOrFreiend ? styles.hide : "" }>
-					{/* <Button className={styles.chatButton} color="primary" variant="solid" onPress={onOpen}>Chats</Button> */}
-					{/* 채팅목록 */}
-					<div className={styles.chats}>
-						{chatRooms?.list?.map((chat :Chat) => {
-							return (
-								<ChatRoom key={chat.roomId} chatId={chat.roomId} badge={chat.cnt} you={chat.participants.filter(member => member !== userInfo.userId)}/>
-							)
-						})
-						}
-					</div>
+				<div className="flex flex-col gap-4 items-center z-20000">
+					<p>회원가입하고 나와 맞는 듀오를 찾아보세요</p>
+					<Friends />
 				</div>
 			</div>
+			{/* 채팅목록창 */}
+			<div className={chatOrFreiend ? styles.hide : "" }>
+				{/* <Button className={styles.chatButton} color="primary" variant="solid" onPress={onOpen}>Chats</Button> */}
+				{/* 채팅목록 */}
+				<div className={styles.chats}>
+					{chatRooms?.list?.map((chat :Chat) => {
+						return (
+							<ChatRoom key={chat.roomId} chatId={chat.roomId} badge={chat.cnt} you={chat.participants.filter(member => member !== userInfo.userId)}/>
+						)
+					})
+					}
+				</div>
+			</div>
+		</div>
+		}
 		</div>
 	);
 };
