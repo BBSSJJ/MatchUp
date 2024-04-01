f43f379561d94cb7c20ddb6610288b6a6cdf06bf
이준학 <0710june@naver.com>
2024-04-01 11:10:21 +0900

    fix: swagger 설정 추가

--- src/main/java/com/ssafy/chat/controller/ChatController.java
+++ src/main/java/com/ssafy/chat/controller/ChatController.java
@@ -6,6 +6,12 @@ import com.ssafy.chat.dto.ListDataDto;
 import com.ssafy.chat.dto.MessageDataDto;
 import com.ssafy.chat.service.ChatService;
 import com.ssafy.chat.util.JwtTokenUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,14 +22,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/chats")
+@RequestMapping("/api/chats")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Chatroom", description = "채팅방 관련 기능입니다.")
 public class ChatController {
 
     private final ChatService chatService;
     private final JwtTokenUtil jwtTokenUtil;
 
+    @Operation(summary = "채팅방 목록 조회", description = "본인 userId가 포함된 채팅방 정보를 리스트로 반환합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "채팅방 리스트", // 응답코드 200일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = ChatRoomDto.class))), // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+    })
     @GetMapping("/rooms")
     public ResponseEntity<?> showChatRooms(HttpServletRequest request) {
 
@@ -32,39 +44,11 @@ public class ChatController {
         return new ResponseEntity<>(new ListDataDto(data), HttpStatus.OK);
     }
 
-    @GetMapping("/rooms/{roomId}")
-    public ResponseEntity<?> showChattings(HttpServletRequest request, @PathVariable String roomId) {
-
-        List<ChatDto> data = chatService.findChattings(jwtTokenUtil.getUserId(request), roomId);
-
-        return new ResponseEntity<>(new ListDataDto(data), HttpStatus.OK);
-    }
-
-    @PostMapping("/rooms")
-    public ResponseEntity<?> createChatRoom(HttpServletRequest request, @RequestBody ChatRoomDto chatRoomDto) {
-
-        try {
-            log.error("input ChatRoomDto : {}", chatRoomDto.toString());
-            chatService.createChatRoom(jwtTokenUtil.getUserId(request), chatRoomDto);
-        } catch (Exception e) {
-            return new ResponseEntity<>(new MessageDataDto(e.getMessage()), HttpStatus.OK);
-        }
-
-        return new ResponseEntity<>(new MessageDataDto("created"), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/rooms/{roomId}")
-    public ResponseEntity<?> deleteChatRoom(HttpServletRequest request, @PathVariable String roomId) {
-
-        try {
-            chatService.deleteChatRoom(jwtTokenUtil.getUserId(request), roomId);
-        } catch (Exception e) {
-            return new ResponseEntity<>(new MessageDataDto(e.getMessage()), HttpStatus.OK);
-        }
-
-        return new ResponseEntity<>(new MessageDataDto("deleted"), HttpStatus.OK);
-    }
-
+    @Operation(summary = "채팅방 정보 조회", description = "상대 userId와 본인 userId가 포함된 채팅방의 정보를 반환합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "채팅방 정보", // 응답코드 200일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = ChatRoomDto.class))), // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+    })
     @GetMapping("/users/{userId}")
     public ResponseEntity<?> showChatRoomWithUser(HttpServletRequest request, @PathVariable Long userId) {
 
@@ -76,5 +60,59 @@ public class ChatController {
 
         return new ResponseEntity<>(chatRoomDto, HttpStatus.OK);
     }
+
+    @Operation(summary = "채팅내역 조회", description = "채팅방 roomId로 해당 채팅방의 채팅 내역을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "채팅 리스트", // 응답코드 200일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = ChatDto.class))), // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+    })
+    @GetMapping("/rooms/{roomId}")
+    public ResponseEntity<?> showChattings(HttpServletRequest request, @PathVariable String roomId) {
+
+        List<ChatDto> data = chatService.findChattings(jwtTokenUtil.getUserId(request), roomId);
+
+        return new ResponseEntity<>(new ListDataDto(data), HttpStatus.OK);
+    }
+
+    @Operation(summary = "채팅방 생성", description = "본인 userId와 상대 userId를 전달하여 채팅방을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "생성 완료 메세지", // 응답코드 200일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = MessageDataDto.class))), // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+            @ApiResponse(responseCode = "500", description = "잘못된 요청(illegal request)", // 응답코드 400일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = MessageDataDto.class))) // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+    })
+    @PostMapping("/rooms")
+    public ResponseEntity<?> createChatRoom(HttpServletRequest request, @RequestBody ChatRoomDto chatRoomDto) {
+
+        try {
+            log.error("input ChatRoomDto : {}", chatRoomDto.toString());
+            chatService.createChatRoom(jwtTokenUtil.getUserId(request), chatRoomDto);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new MessageDataDto(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(new MessageDataDto("created"), HttpStatus.OK);
+    }
+
+    @Operation(summary = "채팅방 삭제", description = "roomId에 해당하는 채팅방을 삭제합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "삭제 완료 메세지", // 응답코드 200일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = MessageDataDto.class))), // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+            @ApiResponse(responseCode = "500", description = "잘못된 요청(illegal request)", // 응답코드 400일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = MessageDataDto.class))) // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+    })
+    @DeleteMapping("/rooms/{roomId}")
+    public ResponseEntity<?> deleteChatRoom(HttpServletRequest request, @PathVariable String roomId) {
+
+        try {
+            chatService.deleteChatRoom(jwtTokenUtil.getUserId(request), roomId);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new MessageDataDto(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(new MessageDataDto("deleted"), HttpStatus.OK);
+    }
+
+
 
 }
--- src/main/java/com/ssafy/chat/controller/RecruitController.java
+++ src/main/java/com/ssafy/chat/controller/RecruitController.java
@@ -1,8 +1,15 @@
 package com.ssafy.chat.controller;
 
 import com.ssafy.chat.dto.ListDataDto;
+import com.ssafy.chat.dto.MessageDataDto;
 import com.ssafy.chat.dto.RecruitDto;
 import com.ssafy.chat.service.RecruitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recruits")
+@RequestMapping("/api/recruits")
 @RequiredArgsConstructor
+@Tag(name = "Recruit", description = "구인게시판 관련 기능입니다.")
 public class RecruitController {
 
     private final RecruitService recruitService;
 
+    @Operation(summary = "구인글 목록 조회", description = "구인글 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "구인글 목록 리스트", // 응답코드 200일때 응답 설명
+                    content = @Content(schema = @Schema(implementation = RecruitDto.class))), // 해당 응답코드에서 어떤 클래스를 응답하는지 작성
+    })
     @GetMapping
     public ResponseEntity<?> showRecruits() {
 
