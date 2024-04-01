c4e81c57b9206e9923311f99b49142feaf5fae6b
sumin <sumin510@naver.com>
2024-04-01 13:36:30 +0900

    fix: chatRoom.tsx

--- src/app/ui/chat/chat.tsx
+++ src/app/ui/chat/chat.tsx
@@ -137,7 +137,7 @@ export default function DirectMessage({roomId} : {roomId :string}) {
 			<div className={styles.chatModal}>
 				<div className={styles.messages}>
 					{/* message 표시 */}
-					{messages.list.map((message, index) => (
+					{messages?.list?.map((message, index) => (
 						<div key={index}  className={`${styles.messageContainer}`}>
 							<div className={`flex ${message?.userId === userInfo.userId ? styles.myMessage : styles.otherMessage}`}>
 								<Image src={message?.iconUrl} width="20px" height="20px" />
