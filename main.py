5a13bea7184c9bfb7f312ce6a5c3e1e5f90c0102
이원석 <epmdrpl213@gmail.com>
2024-04-01 17:55:50 +0900

    feat : 추천 알고리즘 구현 24.04.01 작업

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-from models.check import checkfunc
-from check import checkfunc as two
-
 from fastapi import FastAPI
+
+import heapq
+import joblib
+import pandas as pd
+
+from apis.user import get_user_tier
 
 matchup = FastAPI()
 
+# 승률 기반의 유저 추천
+@matchup.get("/api/recommends/winning/{user_id}")
+async def winning(user_id: int):
+    # 유저 정보 가져오기
+    tier, division = get_user_tier(user_id)
 
-@matchup.get("/api/recommend/1/{user_pk}")
-async def recommendation_1(user_pk: int):
-    return {"users": [1, 2, 3, 4, 5, checkfunc(user_pk)]}
+    # 적당한 모델 불러오기
+    mlp = joblib.load(f'models/winning_models/{tier}_model.pkl')
 
+    # 승률 상위 10명 불러오기
+    probabilities = mlp.predict_proba()
 
-@matchup.get("/api/recommend/2/{user_pk}")
-async def recommendation_2(user_pk: int):
-    return {"users": [6, 7, 8, 9, 10, two(user_pk)]}
+    recommendations = []
+
+    for i, probability in enumerate(probabilities):
+        heapq.heappush(recommendations, (-probability[1], i))
+
+    top_10 = []
+
+    for _ in range(10):
+        top_10.append(heapq.heappop(recommendations))
+
+    return get_user_info(top_10)
+
+# 즐거움 기반의 유저 추천
+@matchup.get("/api/recommends/2/{user_id}")
+async def enjoying(user_id: int):
+    # 유저 정보 가져오기
+
+    # 유사 유저 10명 불러오기
+
+    return get_user_info(6, 7, 8, 9, 10)
+
+# 유저 상세 정보
+@matchup.get("/api/recommends/statistics/{user_id}")
+async def user_info(user_id: int):
+    # 유저 데이터 불러오기
+    user_df = pd.DataFrame()
+
+    tier = 'gold'
+
+    # scaler 불러오기
+    scaler = joblib.load(f"statistics/scalers/{tier}.joblib")
+
+    return {}
