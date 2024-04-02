121e31fcc8be67bed9d72df2bccb95f887bf08da
이원석 <epmdrpl213@gmail.com>
2024-04-02 13:59:23 +0900

    test : enjoying 추천 알고리즘 테스트 DB 연결 확인

---
+++ algorithms/enjoying.py
@@ -0,0 +1,54 @@
+import faiss
+import pymongo
+
+def ten_neighbors(user_list, user_id):
+    puuid_list = []
+
+    for user in user_list:
+        puuid_list.append(user["riotId"])
+
+    # MongoDB 연결
+    client = pymongo.MongoClient("mongodb://matchup.site:3311/")
+
+    # DB 불러오기
+    matchup_statistics_db = client["matchup_statistics_db"]
+    
+    # 해당하는 유저 정보만 불러오기
+    documents = matchup_statistics_db["indicators"].find({"_id": {"$in": puuid_list}})
+
+    # 유저 벡터 만들기
+    user_vectors = []
+
+    for document in documents:
+        total_count = document["metadata"]["totalCount"]
+        time_duration = document["metadata"]["timeDurationAvg"]
+        early_surrender = document["metadata"]["isOurTeamEarlySurrenderedCount"]
+        ping_count = document["metadata"]["pingCountAvg"]
+
+        user_vector = [time_duration, early_surrender / total_count, ping_count]
+
+        user_vectors.append(user_vector)
+
+    # 인덱스 생성
+    index = faiss.IndexFlatL2(3)
+    index.add(user_vectors)
+
+    # 본인 벡터 만들기
+
+    my_document = matchup_statistics_db["indicators"].find_one({"_id": user_id})
+    
+    my_total_count = my_document["metadata"]["totalCount"]
+    my_time_duration = my_document["metadata"]["timeDurationAvg"]
+    my_early_surrender = my_document["metadata"]["isOurTeamEarlySurrenderedCount"]
+    my_ping_count = my_document["metadata"]["pingCountAvg"]
+
+    my_vector = [[my_time_duration, my_early_surrender / my_total_count, my_ping_count]]
+
+    distance_list, index_list = index.search(my_vector, 10)
+
+    user_ids = []
+
+    for index in index_list:
+        user_ids.append(user_list[index]["userId"])
+
+    return user_ids
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import heapq
 import joblib
 import pandas as pd
 
-from apis.user import get_user_tier
+import apis.user
+import algorithms.enjoying
 
 matchup = FastAPI()
 
@@ -12,10 +13,14 @@ matchup = FastAPI()
 @matchup.get("/api/recommends/winning/{user_id}")
 async def winning(user_id: int):
     # 유저 정보 가져오기
-    tier, division = get_user_tier(user_id)
+    tier, division = apis.user.get_user_tier(user_id)
+    divisions = {"I": 1, "II": 2, "III": 3, "IV": 4}
 
-    # 적당한 모델 불러오기
-    mlp = joblib.load(f'models/winning_models/{tier.lower()}_model.pkl')
+    if tier == "DIAMOND":
+        mlp = joblib.load(f'models/winning_models/{tier.lower()}_{divisions[division]}_model.pkl')
+    else:
+        # 적당한 모델 불러오기
+        mlp = joblib.load(f'models/winning_models/{tier.lower()}_model.pkl')
 
     # 승률 상위 10명 불러오기
     probabilities = mlp.predict_proba()
@@ -33,13 +38,52 @@ async def winning(user_id: int):
     return [1, 2, 3, 4, 5]
 
 # 즐거움 기반의 유저 추천
-@matchup.get("/api/recommends/2/{user_id}")
+@matchup.get("/api/recommends/enjoying/{user_id}")
 async def enjoying(user_id: int):
-    # 유저 정보 가져오기
+    # 유저 목록 불러오기
+    user_list = apis.user.get_users(user_id)
 
-    # 유사 유저 10명 불러오기
+    # 유저 평가 데이터 확인
+    my_ratings = apis.user.get_user_ratings(user_id)
 
-    return [6, 7, 8, 9, 10]
+    # 추천 리스트
+    recommendations = []
+
+    if len(my_ratings) < 10:
+        nearest_neighbors = algorithms.enjoying.ten_neighbors(user_list, user_id)
+
+        weight = 1
+        neighbor_weight = 0
+
+        for neighbor in nearest_neighbors:
+            heapq.heappush(recommendations, (weight, neighbor))
+
+            neighbor_ratings = apis.user.get_user_ratings(neighbor)
+
+            for neighbor_rating in neighbor_ratings:
+                score = neighbor_rating["score"]
+
+                if score == 5:
+                    heapq.heappush(recommendations, (neighbor_weight, neighbor_rating["feedbackedUserId"]))
+                    neighbor_weight += 0.1
+            
+            if len(recommendations) >= 10 + weight:
+                break
+            else:
+                weight += 1
+
+    else:
+        pass
+
+    # 유저 정보 반환하기
+    user_infos = []
+
+    for _ in range(10):
+        w, recommendation_user_id = heapq.heappop(recommendations)
+
+        user_infos.append(apis.user.get_user_records_for_recommend(recommendation_user_id))
+
+    return user_infos
 
 # 유저 상세 정보
 @matchup.get("/api/recommends/statistics/{user_id}")
