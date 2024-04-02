5b113dcd987a4a332dac4398d5ad1082e9f9b02b
남상엽 <spearoad15@gmail.com>
2024-04-02 18:23:08 +0900

    fix : 쿠키에 달아주는 name 수정

--- src/main/java/com/ssafy/matchup/user/main/dto/request/UserSnsDto.java
+++ src/main/java/com/ssafy/matchup/user/main/dto/request/UserSnsDto.java
@@ -1,14 +1,26 @@
 package com.ssafy.matchup.user.main.dto.request;
 
 import com.ssafy.matchup.user.main.dto.UserDto;
+import com.ssafy.matchup.user.main.entity.type.AuthorityType;
 import com.ssafy.matchup.user.main.entity.type.SnsType;
+import com.ssafy.matchup.user.riotaccount.api.dto.RiotAccountDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UserSnsDto {
-    private UserDto userDto;
+    private Long userId;
+    private AuthorityType role;
+    private RiotAccountDto riotAccount;
     private String snsId;
     private SnsType snsType;
+
+    public UserSnsDto(UserDto userDto, String snsId, SnsType snsType) {
+        this.userId = userDto.getUserId();
+        this.role = userDto.getRole();
+        this.riotAccount = userDto.getRiotAccount();
+        this.snsId = snsId;
+        this.snsType = snsType;
+    }
 }
