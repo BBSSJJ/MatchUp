ecfd67c53d46c793abb81b96d8dffa31b0afcfa6
이원석 <epmdrpl213@gmail.com>
2024-04-03 06:02:20 +0900

    feat : 유저 상세 정보 유저 실력 분포 출력 추가

--- apis/user.py
+++ apis/user.py
@@ -1,5 +1,6 @@
 import requests
 import pymongo
+import pandas as pd
 
 def get_user_puuid(user_id: int):
     response = requests.get(f'https://matchup.site/api/users/{user_id}').json()
@@ -54,3 +55,45 @@ def get_user_profile(user_id: int):
     user_record["losses"] = summoners_document["league"]["losses"]
 
     return user_record
+
+
+def get_user_indicator(user_id: int):
+    # MongoDB 연결
+    client = pymongo.MongoClient("mongodb://mongodb-statistics-service:3311/")
+
+    # DB 불러오기
+    matchup_statistics_db = client["matchup_statistics_db"]
+
+    # 해당하는 유저 정보만 불러오기
+    document = matchup_statistics_db["indicators"].find_one({"_id": get_user_puuid(user_id)})
+
+    match_indicator = document["matchIndicatorStatistics"]
+
+    # 유저 지표 불러오기
+
+    user_indicator = {}
+
+    user_indicator["csDiffer"] = match_indicator["laneIndicatorAvg"]["basicWeight"]["csDiffer"]
+    user_indicator["expDiffer"] = match_indicator["laneIndicatorAvg"]["basicWeight"]["expDiffer"]
+    user_indicator["turretPlateDestroyDiffer"] = match_indicator["laneIndicatorAvg"]["basicWeight"]["turretPlateDestroyDiffer"]
+    user_indicator["dealDiffer"] = match_indicator["laneIndicatorAvg"]["aggresiveLaneAbilility"]["dealDiffer"]
+
+    user_indicator["turretKillsPerDeaths"] = match_indicator["macroIndicatorAvg"]["splitPoint"]["turretKillsPerDeaths"]
+    user_indicator["damageDealtToTurretsPerTeamTotalTowerDamageDone"] = match_indicator["macroIndicatorAvg"]["splitPoint"]["damageDealtToTurretsPerTeamTotalTowerDamageDone"]
+
+    user_indicator["totalTimeCCingOthersPerTotalDamageTaken"] = match_indicator["macroIndicatorAvg"]["initiatingPoint"]["totalTimeCCingOthersPerTotalDamageTaken"]
+    user_indicator["totalDamageTakenPerTeamTotalDamageTaken"] = match_indicator["macroIndicatorAvg"]["initiatingPoint"]["totalDamageTakenPerTeamTotalDamageTaken"]
+    user_indicator["damageSelfMitigatedPerTotalDamageTaken"] = match_indicator["macroIndicatorAvg"]["initiatingPoint"]["damageSelfMitigatedPerTotalDamageTaken"]
+
+    user_indicator["visionScorePerDeath"] = match_indicator["macroIndicatorAvg"]["visionPoint"]["visionScorePerDeath"]
+    user_indicator["totalJungleObjectivePerGameDuration"] = match_indicator["macroIndicatorAvg"]["jungleHoldPoint"]["totalJungleObjectivePerGameDuration"]
+
+    user_indicator["getObjectiveDifferPerGameDuration"] = match_indicator["macroIndicatorAvg"]["objectivePoint"]["getObjectiveDifferPerGameDuration"]
+
+    user_indicator["damagePerMinute"] = match_indicator["macroIndicatorAvg"]["totalDealPoint"]["damagePerMinute"]
+    user_indicator["dealPerGold"] = match_indicator["macroIndicatorAvg"]["totalDealPoint"]["dealPerGold"]
+    user_indicator["teamDamagePercentage"] = match_indicator["macroIndicatorAvg"]["totalDealPoint"]["teamDamagePercentage"]
+
+    user_indicator_df = pd.DataFrame(user_indicator)
+
+    return user_indicator_df
