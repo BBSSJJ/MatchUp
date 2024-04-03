b20a8bcbc419683ac78e7b5586a1f8669ec8cd8c
이원석 <epmdrpl213@gmail.com>
2024-04-03 13:28:26 +0900

    fix : 유저 상세 지표 오류 수정 df 처리 방식 변경

--- b.py
+++ b.py
@@ -5,9 +5,11 @@ import scipy.stats
 import pymongo
 import a
 
-scaler = joblib.load("statistics/scalers/gold_scaler.joblib")
+scaler = joblib.load("statistics/scalers/diamond_1_scaler.joblib")
 
-scaled_data = scaler.transform(a.a1())
+b1 = pd.DataFrame([a.a1()])
+
+scaled_data = scaler.transform(b1)
 
 percentiles = np.round(scipy.stats.norm.cdf(scaled_data) * 100, 2)
 
