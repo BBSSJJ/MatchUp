eb67d8f83e2e7b70a140b1e69702a6f4ffa4fd39
이원석 <epmdrpl213@gmail.com>
2024-04-03 14:12:04 +0900

    feat : 유저 상세 지표 유저 상세 지표 기능 추가

--- b.py
+++ b.py
@@ -4,6 +4,8 @@ import numpy as np
 import scipy.stats
 import pymongo
 import a
+import heapq
+import apis.user
 
 scaler = joblib.load("statistics/scalers/diamond_1_scaler.joblib")
 
@@ -13,5 +15,15 @@ scaled_data = scaler.transform(b1)
 
 percentiles = np.round(scipy.stats.norm.cdf(scaled_data) * 100, 2)
 
+user_keywords = []
+keywords = []
+
 for index, percentile in enumerate(percentiles[0]):
-    print(index, percentile)
+    if 0 <= index < 4:
+        user_keywords.append(apis.user.get_user_keyword(index, percentile))
+    else:
+        heapq.heappush(keywords, (-percentile, apis.user.get_user_keyword(index, percentile)))
+
+for _ in range(2):
+    print(heapq.heappop(keywords))
+    print(heapq.heappop(keywords)[1])
