ae95b3770b928b2da523ee0dd903dec7abe82a10
이연우 <ckadltmf0224@naver.com>
2024-04-04 09:58:45 +0900

    css

--- src/app/onmatchup/[sessionId]/page.tsx
+++ src/app/onmatchup/[sessionId]/page.tsx
@@ -30,6 +30,7 @@ export default function Page({ params }: { params: { sessionId: string }}) {
   // const [recordingId, setRecordingId] = useState<string>('')
 
   const [userInfo, setUserInfo] = useAtom(userInfoAtom)
+  const [onAudio, setOnAudio] = useState<boolean>(true)
 
   async function getSession(sessionId: string) {
     const response = await axios.get(APPLICATION_SERVER_URL + 'api/sessions/' + sessionId, {
@@ -178,6 +179,11 @@ export default function Page({ params }: { params: { sessionId: string }}) {
   //   window.alert('녹화 시작')
   //   return setRecordingId(response.data.id)
   // }
+
+  function micChange() {
+    setOnAudio(!onAudio)
+    openvidu.publiser.publishAudio(onAudio)
+  }
   
 
   return (
@@ -209,13 +215,16 @@ export default function Page({ params }: { params: { sessionId: string }}) {
           )}
           <div className="w-screen">
             <MatchupChats roomId={params.sessionId} />
-            <div className="flex justify-center pt-10">
+            <div className="flex justify-center pt-3">
               <Link href={'/lobby'} onClick={leaveSession}>
                 <Button>나가기</Button>
               </Link>
               {/* <Button onPress={publishScreenShare}>화면 공유</Button>
               <Button color="danger" onPress={() => startRecording(params.sessionId)}>녹화하기</Button>
               <Button color="warning" onPress={() => stopRecording(recordingId)}>녹화중지</Button> */}
+            </div>
+            <div className="flex justify-center pt-3">
+              <Button onPress={micChange}>{(onAudio ? (<svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -100 500 700"><path d="M96 96V256c0 53 43 96 96 96s96-43 96-96H208c-8.8 0-16-7.2-16-16s7.2-16 16-16h80V192H208c-8.8 0-16-7.2-16-16s7.2-16 16-16h80V128H208c-8.8 0-16-7.2-16-16s7.2-16 16-16h80c0-53-43-96-96-96S96 43 96 96zM320 240v16c0 70.7-57.3 128-128 128s-128-57.3-128-128V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v24z"/></svg>) : (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L472.1 344.7c15.2-26 23.9-56.3 23.9-88.7V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v24 16c0 21.2-5.1 41.1-14.2 58.7L416 300.8V256H358.9l-34.5-27c2.9-3.1 7-5 11.6-5h80V192H336c-8.8 0-16-7.2-16-16s7.2-16 16-16h80V128H336c-8.8 0-16-7.2-16-16s7.2-16 16-16h80c0-53-43-96-96-96s-96 43-96 96v54.3L38.8 5.1zM358.2 378.2C346.1 382 333.3 384 320 384c-70.7 0-128-57.3-128-128v-8.7L144.7 210c-.5 1.9-.7 3.9-.7 6v40c0 89.1 66.2 162.7 152 174.4V464H248c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H344V430.4c20.4-2.8 39.7-9.1 57.3-18.2l-43.1-33.9z"/></svg>))}</Button>
             </div>
           </div>
           {openvidu.subscribers.length !== 0 ? (openvidu.subscribers.map((sub: any) => (
